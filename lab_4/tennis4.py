0db52f81ef0b357aafb3f89f6c0f3423e18c38d0
VladaGusakova <163380239+VladaGusakova@users.noreply.github.com>
2025-12-16 21:17:08 +0500

    Unnecessary methods have been removed
    In fact, it's almost the same as tennis2

--- lab_4/tennis4.py
+++ lab_4/tennis4.py
@@ -4,7 +4,6 @@ class TennisGame4:
         self.receiver = player2_name
         self.server_score = 0
         self.receiver_score = 0
-        self.scores = ["Love", "Fifteen", "Thirty", "Forty"]
 
     def won_point(self, player_name):
         if self.server == player_name:
@@ -13,37 +12,20 @@ class TennisGame4:
             self.receiver_score += 1
 
     def score(self):
-        if self.server_has_won():
-            return f"Win for {self.server}"
-        if self.receiver_has_won():
-            return f"Win for {self.receiver}"
-            
-        if self.server_has_advantage():
-            return f"Advantage {self.server}"
-        if self.receiver_has_advantage():
-            return f"Advantage {self.receiver}"
-            
-        if self.is_deuce():
-            return "Deuce"
-            
-        server_score_str = self.scores[self.server_score]
-        receiver_score_str = self.scores[self.receiver_score]
-        
+        scores = ["Love", "Fifteen", "Thirty", "Forty"]
+
+        if self.server_score >= 3 and self.receiver_score >= 3:
+            if self.server_score == self.receiver_score:
+                return "Deuce"
+            elif self.server_score - self.receiver_score == 1:
+                return f"Advantage {self.server}"
+            elif self.receiver_score - self.server_score == 1:
+                return f"Advantage {self.receiver}"
+            elif self.server_score - self.receiver_score >= 2:
+                return f"Win for {self.server}"
+            elif self.receiver_score - self.server_score >= 2:
+                return f"Win for {self.receiver}"
+
         if self.server_score == self.receiver_score:
-            return f"{server_score_str}-All"
-        return f"{server_score_str}-{receiver_score_str}"
-
-    def receiver_has_advantage(self):
-        return self.receiver_score >= 4 and (self.receiver_score - self.server_score) == 1
-
-    def server_has_advantage(self):
-        return self.server_score >= 4 and (self.server_score - self.receiver_score) == 1
-
-    def receiver_has_won(self):
-        return self.receiver_score >= 4 and (self.receiver_score - self.server_score) >= 2
-
-    def server_has_won(self):
-        return self.server_score >= 4 and (self.server_score - self.receiver_score) >= 2
-
-    def is_deuce(self):
-        return self.server_score >= 3 and self.receiver_score >= 3 and self.server_score == self.receiver_score
+            return f"{scores[self.server_score]}-All"
+        return f"{scores[self.server_score]}-{scores[self.receiver_score]}"
